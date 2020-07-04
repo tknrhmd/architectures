463785ed6f6ac9f79b9d0207e87750b5d0b5dc99
tknrhmd <67689724+tknrhmd@users.noreply.github.com>
2020-07-05 03:31:45 +0900

    react tutorial

--- separation/bff/src/app/backendsforfrontends.ts
+++ separation/bff/src/app/backendsforfrontends.ts
@@ -1,3 +1,15 @@
 /***************************************************************
  * Aggregate BFF-related logic, such as requests to APIs, here *
  ***************************************************************/
+/*
+ * ex)
+ * - API Gateway
+ *  - API Aggregation
+ *  - API Translation
+ *  - API Cashe
+ *  - API Filter
+ * - Server Side Rendering
+ * - Session Management
+ * - File Upload
+ * - WebSocket、Server Sent Events、Long Polling
+ */
--- separation/frontend/src/index.js
+++ separation/frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MyButton from './components/MyButton'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <MyButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
